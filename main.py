00d0a97279d0d68de0cc8b937f3876f7d44cbd74
Yash Shrivastav <153893232+Yash010704@users.noreply.github.com>
2025-05-17 14:26:05 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import tkinter as tk
+from gui.app import TradingSimulatorApp
 
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.geometry("900x600")
+    app = TradingSimulatorApp(root)
+    root.mainloop()
